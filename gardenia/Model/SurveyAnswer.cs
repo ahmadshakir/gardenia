f244b360203708eedf42ebffd86e27fb9df7db75
ahmad shakir <ahkay13@gmail.com>
2023-10-19 00:15:40 +0800

    Model

---
+++ gardenia/Model/SurveyAnswer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace gardenia.Model;
+
+public partial class SurveyAnswer
+{
+    public int Id { get; set; }
+
+    public int SurveysFk { get; set; }
+
+    public string Answer { get; set; } = null!;
+
+    public virtual Survey SurveysFkNavigation { get; set; } = null!;
+}
---
+++ gardenia/Model/SurveyQuestion.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace gardenia.Model;
+
+public partial class SurveyQuestion
+{
+    public int Id { get; set; }
+
+    public string Question { get; set; } = null!;
+}
